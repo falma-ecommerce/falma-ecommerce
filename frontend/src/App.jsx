c3a198e5241e4c0f17219d92926f14e50bc6d8ab
Linda <lindaroy202@gmail.com>
2022-06-06 16:23:44 +0200

    *modified packages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <main>
+      <Header />
      <h1>It's FALMA </h1>
      <h1>Check Out falma Sales✨  </h1>
+
+
     </main>
   );
 }
