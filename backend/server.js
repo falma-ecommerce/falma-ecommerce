6c057c3fe58f5af597c90f1909f9b5ec14f1614b
Freeman <pfreejohnson@gmail.com>
2022-06-14 11:42:49 +0200

    Update 2

--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,6 @@ import data from './data.js'
 const app = express();
 app.use(express.json());
 
-const options = {
-  method: 'GET',
-  url: 'https://asos2.p.rapidapi.com/products/v3/detail',
-  params: {id: '9851612', lang: 'en-US', store: 'US', sizeSchema: 'US', currency: 'USD'},
-  headers: {
-    'X-RapidAPI-Host': 'asos2.p.rapidapi.com',
-    'X-RapidAPI-Key': 'd8619d8c30mshd90d561a87bd52bp1e5406jsn038635ae051c'
-  }
-};
-
-// axios.request(options).then(function (response) {
-// 	console.log(response.data);
-// }).catch(function (error) {
-// 	console.error(error);
-// });
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
