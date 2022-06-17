c124501e24bf26688409adf43d0b621f0dd448a6
Linda <lindaroy202@gmail.com>
2022-06-17 16:15:06 +0200

    *created a new branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen/HomeScreen";
-<<<<<<< HEAD
-import WomenCloths from "./components/Home/Categories/women/WomenCloth";
-=======
-import AllProductScreen from "./screens/AllProductScreen/AllProductsScreen"
-import ProductDetailsScreen from "./screens/ProductDetailsScreen/ProductDetailScreen"
->>>>>>> 451b6bab585018dfbb156f1038bcffdc1d86b8b8
+import AllProductScreen from "./screens/AllProductScreen/AllProductsScreen";
+import ProductDetailsScreen from "./screens/ProductDetailsScreen/ProductDetailScreen";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -17,14 +13,12 @@ const App = () => {
         <main>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
-<<<<<<< HEAD
-            <Route path="/women-cloths" element={<WomenCloths />} />
+            <Route path="/all-products" element={<AllProductScreen />} />
+            <Route
+              path="/product-details/:id"
+              element={<ProductDetailsScreen />}
+            />
           </Routes>
-=======
-            <Route path="/all-products" element={<AllProductScreen />}/>
-            <Route path="/product-details/:id" element={<ProductDetailsScreen />}/>
-          </Routes> 
->>>>>>> 451b6bab585018dfbb156f1038bcffdc1d86b8b8
         </main>
       </div>
     </BrowserRouter>
