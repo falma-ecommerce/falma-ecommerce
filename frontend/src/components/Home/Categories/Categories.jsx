ba49fa24f31d892904f34346e89caf9577dfe8be
Linda <lindaroy202@gmail.com>
2022-07-12 21:44:31 +0200

    *files were created and some deleted

--- frontend/src/components/Home/Categories/Categories.jsx
+++ frontend/src/components/Home/Categories/Categories.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import mensuit from "../../../components/images/mensuit1.webp"
+import menshoes from "../../../components/images/menshoes1.webp"
+import womensuit from "../../../components/images/womensuit1.webp"
+import womenshoes from "../../../components/images/womenshoes1.webp"
+import kids from "../../../components/images/kids1.webp"
+import kidshoes from "../../../components/images/kidshoes1.webp"
 import "./Categories.modules.css";
 
 const Categories = () => {
@@ -12,12 +17,14 @@ const Categories = () => {
             className="items"
             style={{
               backgroundImage:
-                "url(" +
-                "https://img01.ztat.net/article/spp-media-p1/c335a45df8d1426fbdcecb8eaf9dc5ba/53d764af5aad4167819846e85771c60c.jpg?imwidth=400" +
-                ")",
+               `url(${mensuit})`,
               minWidth: "19vw",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              
               height: "60vh",
             }}
+            // "https://img01.ztat.net/article/spp-media-p1/c335a45df8d1426fbdcecb8eaf9dc5ba/53d764af5aad4167819846e85771c60c.jpg?imwidth=400" +
           >
             <div className="one"></div>
             <div className="details">
@@ -33,11 +40,11 @@ const Categories = () => {
             className="items"
             style={{
               backgroundImage:
-                "url(" +
-                "https://img01.ztat.net/article/spp-media-p1/cb5586ee0ea54d9fb5899574abef1d59/d630ce0d90154cd291f0eb48b604cd29.jpg?imwidth=400" +
-                ")",
-              minWidth: "19vw",
-              height: "60vh",
+              `url(${womensuit})`,
+             minWidth: "19vw",
+             height: "60vh",
+             backgroundPosition: "center",
+             backgroundSize: "cover",
             }}
           >
             <div className="two"></div>
@@ -54,11 +61,11 @@ const Categories = () => {
             className="items"
             style={{
               backgroundImage:
-                "url(" +
-                "https://img01.ztat.net/article/spp-media-p1/5a8718cd90074055a92e09b5b3a23bbe/348dd2baae514da89c21fb6e5174fadf.jpg?imwidth=420" +
-                ")",
-              minWidth: "19vw",
-              height: "60vh",
+              `url(${kids})`,
+             minWidth: "19vw",
+             height: "60vh",
+             backgroundPosition: "center",
+             backgroundSize: "cover",
             }}
           >
             <div className="three"></div>
@@ -75,11 +82,11 @@ const Categories = () => {
             className="items"
             style={{
               backgroundImage:
-                "url(" +
-                "https://img01.ztat.net/article/spp-media-p1/6eb1c861b03044c69a2334f806d1c41a/34749447eaf34610b4bf09adbb5ef239.jpg?imwidth=400" +
-                ")",
-              minWidth: "19vw",
-              height: "60vh",
+              `url(${menshoes})`,
+             minWidth: "19vw",
+             height: "60vh",
+             backgroundPosition: "center",
+             backgroundSize: "cover",
             }}
           >
             <div className="four"></div>
@@ -96,11 +103,11 @@ const Categories = () => {
             className="items"
             style={{
               backgroundImage:
-                "url(" +
-                "https://img01.ztat.net/article/spp-media-p1/f374c54bc39b4eef9f495ec86d8d4887/f4c68990c5444735b67964e9d7cf7436.jpg?imwidth=430" +
-                ")",
-              minWidth: "19vw",
-              height: "60vh",
+              `url(${womenshoes})`,
+             minWidth: "19vw",
+             height: "60vh",
+             backgroundPosition: "center",
+             backgroundSize: "cover",
             }}
           >
             <div className="five"></div>
@@ -117,11 +124,11 @@ const Categories = () => {
             className="items"
             style={{
               backgroundImage:
-                "url(" +
-                "https://img01.ztat.net/article/spp-media-p1/14399ddb1dff4a1083807666555f5184/a6b181af58c44e38876b365b88d0c72d.jpg?imwidth=400" +
-                ")",
-              minWidth: "19vw",
-              height: "60vh",
+              `url(${kidshoes})`,
+             minWidth: "19vw",
+             height: "60vh",
+             backgroundPosition: "center",
+             backgroundSize: "cover",
             }}
           >
             <div className="six"></div>
